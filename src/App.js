94bba1a49acfd3fa0bfcbc4fcbb0556090ace496
sodreamon <60939443+sodreamon@users.noreply.github.com>
2020-03-17 21:47:35 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import { createPortal } from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Portals extends Component {
+  render() {
+    return createPortal(<Message />, document.getElementById("touchme"));
+  }
+}
+
+const Message = () => "Just touched it";
+
+class ReturnTypes extends Component {
+  render() {
+    return "hello";
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <ReturnTypes />
+        <Portals />
+      </Fragment>
+    );
+  }
 }
 
 export default App;
