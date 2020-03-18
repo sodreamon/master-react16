caede0c19e4b2cf5967ad07df3263a1c1dde2d36
sodreamon <60939443+sodreamon@users.noreply.github.com>
2020-03-18 15:09:38 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,62 +1,8 @@
 import React, { Component, Fragment } from "react";
-import { createPortal } from "react-dom";
-
-class ErrorMaker extends Component {
-  state = {
-    friends: ["qwe", "asd", "zxc", "rty"]
-  };
-
-  componentDidMount = () => {
-    setTimeout(() => {
-      this.setState({
-        friends: undefined
-      });
-    }, 2000);
-  };
-
-  render() {
-    const { friends } = this.state;
-
-    return friends.map(friend => `${friend} `);
-  }
-}
-
-class Portals extends Component {
-  render() {
-    return createPortal(<Message />, document.getElementById("touchme"));
-  }
-}
-
-const Message = () => "Just touched it";
-
-class ReturnTypes extends Component {
-  render() {
-    return "hello";
-  }
-}
-
-const ErrorFallback = () => "sorry something went wrong";
 
 class App extends Component {
-  state = {
-    hasError: false
-  };
-
-  componentDidCatch = (error, info) => {
-    console.log(`catched ${error} the info i have ${JSON.stringify(info)}`);
-    this.setState({ hasError: true });
-  };
-
   render() {
-    const { hasError } = this.state;
-
-    return (
-      <Fragment>
-        <ReturnTypes />
-        <Portals />
-        {hasError ? <ErrorFallback /> : <ErrorMaker />}
-      </Fragment>
-    );
+    return <Fragment></Fragment>;
   }
 }
 
